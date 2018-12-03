cb684132a83e93dbb52195809f3f7f2d98596954
Vadhil Amadiz <vamadiz@gmail.com>
2018-12-03 13:41:29 -0500

    Initial commit of entire project

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+BUGGER! A spin on the classic arcade game Frogger!
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+The object of this game is to avoid the bugs going across the screen and reaching the water at the top of screen. Simple! Just move your character by using the left, right, down, and up keys. If you touch a bug you are to start again.
+
+To run this game find the index.html file in the project folder and open it with your preferred web browser.
+
+Good luck!
--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -14,7 +16,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x < 500){
+      this.x = this.x + (this.speed)*dt;
+    } else {
+      this.speed = Math.floor((Math.random()*100) + 100);
+      this.x = -100;
+    }
 };
+
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -24,11 +34,129 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+ this.sprite = 'images/char-boy.png';
+ this.x = 200;
+ this.y = 410;
+};
 
+//Player health. Player gets 3 lives.
+//var health = 3;
+//new Text({
+//text: () => "Health: " + health,
+//  x: -200,
+//  y: 410,
+//  size: 50
+//})
+
+//Code for collision detection between the Player and Enemies
+Player.prototype.update = function(dt) {
+  for (let enemy of allEnemies) {
+    let dx = this.x - enemy.x - 20;
+    let dy = this.y - enemy.y - 20;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < 56) {
+      console.log('hit');
+      this.y = 410;
+      //health -= 1;
+    }
+  }
+  if (this.y < 10) {
+    console.log('You did it!');
+    this.y = 410;
+  }
+};
+
+//Draws the Player on the screen.
+Player.prototype.render = function() {
+ ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//Code for player movement & keeping player from going off screen
+Player.prototype.handleInput = function(obj) {
+  if (this.x > 0){
+		if (obj === 'left'){
+			if ( this.allowableSpace(obj)) {
+				this.x = this.x -50;
+		   }
+	  }
+  }
+
+	if(this.x <= 400){
+		if (obj === 'right'){
+			if (this.allowableSpace(obj)) {
+			this.x = this.x +50;
+			}
+		}
+	}
+
+	if (this.y >= 0 ){
+		if (obj === 'up'){
+			if (this.allowableSpace(obj)) {
+				this.y = this.y -50;
+			}
+		}
+	}
+	if(this.y < 400 ){
+		if (obj === 'down'){
+			if (this.allowableSpace(obj)) {
+				this.y = this.y +50;
+			}
+		}
+	}
+};
+
+
+Player.prototype.allowableSpace = function(direction) {
+	var leftFull = false;
+	var rightFull = false;
+	var upFull = false;
+	var downFull = false;
+  switch(direction) {
+          case ("left"):
+              if (leftFull) {
+                  return false;
+              }
+              else {
+                  return true;
+              }
+              break;
+          case ("right"):
+              if (rightFull) {
+                  return false;
+              }
+              else {
+                  return true;
+              }
+              break;
+          case ("up"):
+              if (upFull) {
+                  return false;
+              }
+              else {
+                  return true;
+              }
+              break;
+          case ("down"):
+              if (downFull) {
+                  return false;
+              }
+              else {
+                  return true;
+              }
+              break;
+          default:
+              return true;
+
+  };
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [new Enemy(-200, 65), new Enemy(-150, 145), new Enemy(-100, 230)];
+
+var player = new Player();
 
 
 
